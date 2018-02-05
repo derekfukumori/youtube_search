8dd5ee49d80c92dae94975dbd33c2ed2d0a4c001
Derek Fukumori <derekfukumori@Dereks-MacBook-Pro.local>
2018-02-05 14:21:03 -0800

    • Implementing search by Internet Archive item

--- iametadata.py
+++ iametadata.py
@@ -1,3 +1,16 @@
+import internetarchive
+import os.path
+
+# def get_ia_metadata(item_id):
+#     print("ffff")
+#     return internetarchive.get_item(item_id)
+
+audio_extensions = ['.mp3', '.flac', '.ogg']
+
+def get_audio_files(item):
+    return [f for f in item.item_metadata['files'] if f['source'] == 'original'\
+            and os.path.splitext(f['name'])[1] in audio_extensions]
+
 def get_file_metadata(item, filename):
     for file_metadata in item.item_metadata['files']:
         if file_metadata['name'] == filename:
--- search.py
+++ search.py
@@ -1,4 +1,6 @@
 import argparse
+import sys
+import iametadata as ia
 from youtube_search import YouTubeSearchManager
 from video_ranking import *
 from internetarchive import get_item
@@ -15,20 +17,13 @@ def search_by_file_metadata(youtube, item, file_metadata):
     return rank_videos(youtube.search(query), item, file_metadata)
 
 def search_by_filename(youtube, item, filename):
-    for file_metadata in item.item_metadata['files']:
-        if file_metadata['name'] == filename:
-            return search_by_file_metadata(youtube, item, file_metadata)
-    return '0'
+    return search_by_file_metadata(youtube, item, ia.get_file_metadata(filename))
 
 def search_by_item(youtube, item):
-    #TODO: Which is the more efficient short circuit?
-    pass
-
-def get_file_metadata(item, filename):
-    for file_metadata in item.item_metadata['files']:
-        if file_metadata['name'] == filename:
-            return file_metadata
-    return {}
+    results = {}
+    for fm in ia.get_audio_files(item):
+        results[fm['name']] = search_by_file_metadata(youtube, item, fm)
+    return results
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fetch and JSONify file\
@@ -37,6 +32,8 @@ if __name__ == "__main__":
             default=IDENTIFIERS_FILE, help='Input CSV file')
     parser.add_argument('-o', '--output', dest='output_file', metavar='OUTPUTFILE',
             default=METADATA_FILE, help='Output path')
+    # parser.add_argument('-sf', '--searchbyfilename', dest='search_by_filename',
+    #         action='store_true', default=False)
     parser.add_argument('-r', '--userange', dest='use_range',
             action='store_true', default=False)
     parser.add_argument('-rs', '--rangestart', dest='range_start', metavar='RANGESTART',
@@ -46,48 +43,40 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
-        ids = open(args.input_file)
+        with open(args.input_file) as f:
+            items = f.readlines()
     except IOError:
-        print("Error: Could not read input file.")
+        print("Error: Could not read input file.", file=sys.stderr)
         exit(1)
 
     #TODO: Results cache as argument
     yt = YouTubeSearchManager(GOOGLE_API_KEY, "data/wcd-rando-1000-yt-cache.json")
     yt_results = {}
 
-    found = 0
-    not_found = 0
-
     #outfile = open("data/wcd-rando-1000-results.tsv", 'w')
     outfile = open(args.output_file, 'w')
 
-    items = ids.readlines()
     rs = args.range_start if args.use_range else 0
     re = args.range_end + 1 if args.use_range else len(items)
 
-    n_acoustids = 0
 
     for i in range(rs,re):
         iaid,filename_url = items[i].rstrip().split(',')
         filename = unquote(filename_url)
 
-        print("Searching " + iaid + '/' + filename + '...')
+        print('Searching ' + iaid + '/' + filename + '...')
 
         if iaid not in yt_results:
             yt_results[iaid] = {}
 
-        #TODO: Sometimes get_item fails, causing a KeyError when
-        # metadata['files'] is accessed. Determine the value of ia_item when
-        # the failure occurs, and check validity before searching.
+        #TODO: Sometimes internetarchive.get_item() fails(?) without raising an
+        # exception, causing a KeyError when metadata['files'] is accessed.
+        # Determine the value of ia_item when the failure occurs, and check validity before searching.
         try:
             ia_item = get_item(iaid)
         except requests.exceptions.ConnectionError:
-            print("Error: Could not retrieve metadata")
-            ids.close()
-            exit(1)
-
-        if get_acoustid(get_file_metadata(ia_item, filename)):
-            n_acoustids += 1
+            print('Error: Could not retrieve metadata for ' + iaid + '.', file=sys.stderr)
+            continue
 
         # yt_results[iaid][filename] = search_by_filename(yt, ia_item, filename)
         # if yt_results[iaid][filename] == '0':
@@ -95,12 +84,11 @@ if __name__ == "__main__":
         # else:
         #     found += 1
         #
-        # file_metadata = get_file_metadata(ia_item, filename)
+        # file_metadata = ia.get_file_metadata(ia_item, filename)
         #
         # url = 'https://www.youtube.com/watch?v=' + yt_results[iaid][filename] if yt_results[iaid][filename] != '0' else ''
         # outfile.write(iaid + '/' + filename_url + '\t' + file_metadata['artist'] + '\t' + file_metadata['title'] + '\t' + url + '\n')
 
     #yt.write_cache_to_disk()
-    print(n_acoustids)
     #TODO: Write results to disk.
     outfile.close()
