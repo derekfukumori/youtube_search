8c397a6864166852a0038ac80e12c7d73e0f7c6c
Derek Fukumori <derekfukumori@Dereks-MacBook-Pro.local>
2018-02-05 18:03:06 -0800

    • Minor changes to YouTube search cache logic • YouTubeSearchManager documentation

--- youtube_search.py
+++ youtube_search.py
@@ -5,7 +5,14 @@ from isodate import parse_duration
 from defaults import *
 
 class YouTubeSearchManager:
-    """Handles YouTube video queries
+    """Handles YouTube video searches.
+
+    Args:
+        api_key      (str): Google API key.
+        max_results  (int): Maximum number of video results to be returned for a
+                            given query.
+        use_cache   (bool): Flag indicating whether to use the search cache.
+        cache_path   (str): Path to the search cache file.
     """
     def __init__(self, api_key, max_results=10, use_cache='False', cache_path=''):
         self._cache_path = cache_path
@@ -23,9 +30,22 @@ class YouTubeSearchManager:
 
     #TODO: Case-insensitive query caching.
     def search(self, query):
+        """Performs a YouTube search for the given query string.
 
-        if self._use_cache and query in self._cache:
-            return self._cache[query]
+        If caching is enabled, search will first attempt to return a cached copy
+        of the results for the given query, and will cache the results if no
+        such results already exist.
+
+        Args:
+            query (str): The query string.
+
+        Returns:
+            A list of dicts corresponding to YouTube videos. Each dict contains
+            fields 'id', 'title', 'duration' and 'description'.
+        """
+        if self._use_cache and query in self._cache\
+        and len(self._cache[query]) >= self._max_results:
+            return self._cache[query][:self._max_results]
 
         response = self._youtube.search().list(
             q = query,
