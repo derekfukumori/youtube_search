53ea2c2bae2ca0f6c5878b2daab8ad873facb914
Derek Fukumori <derekfukumori@Dereks-MacBook-Pro.local>
2018-03-27 20:15:11 -0700

    Switching from pytube to youtube-dl initial implementation

--- ytsearch/youtube_download.py
+++ ytsearch/youtube_download.py
@@ -1,9 +1,11 @@
 import sys
+import glob
 import subprocess
 from os import remove
 from os.path import exists, isfile
 import pytube
 import audioread
+
 
 #TODO: retries (retrying library?).
 def download_audio_file(yt_id, audio_dir, video_dir):
@@ -21,41 +23,58 @@ def download_audio_file(yt_id, audio_dir, video_dir):
         The path of the downloaded file, or a blank stream on unsuccessful
         download or FFmpeg conversion.
     """
+
+    output_template = audio_dir.rstrip('/') + '/' +  yt_id + '.%(ext)s'
     try:
-        yt = pytube.YouTube('https://www.youtube.com/watch?v=' + yt_id)
-    except pytube.exceptions.RegexMatchError:
-        print('Error: Could not download audio stream. Invalid YouTube video id '\
-         + yt_id, file=sys.stderr)
-        return ''
+        subprocess.run(['youtube-dl', '-q', '-o', output_template,
+                       '-f', 'bestaudio', 'https://www.youtube.com/watch?v=' + yt_id],
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError:
+        pass
+    finally:
+        pass
 
-    #TODO: Determine which stream to prioritize; Error handling.
-    stream = yt.streams.filter(only_audio=True).first()
+    dl_files = glob.glob(audio_dir.rstrip('/') + '/' +  yt_id + '.*')
+    if dl_files:
+        return dl_files[0]
 
-    if stream:
-        stream.download(audio_dir, yt_id)
-        audio_path = audio_dir.rstrip('/') + '/' + yt_id + '.' \
-                     + get_stream_extension(stream)
-    else:
-        #Download and extract audio from video stream.
-        #TODO: Determine which stream to prioritize; Error handling.
-        stream = yt.streams.first()
-        stream.download(video_dir, yt_id)
-        video_path = video_dir.rstrip('/') + '/' + yt_id + '.' \
-                     + get_stream_extension(stream)
-        audio_path =  audio_dir.rstrip('/') + '/' + yt_id + '.' + get_stream_extension(stream)
-        try:
-            subprocess.run(['ffmpeg', '-i', video_path, '-vn', '-y', '-acodec', 'copy', \
-            audio_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-        except subprocess.CalledProcessError:
-            return ''
-        finally:
-            remove(video_path)
+    return ''
 
-    if is_audio_valid(audio_path):
-        return audio_path
-    else:
-        #TODO: Retries
-        return ''
+    # try:
+    #     yt = pytube.YouTube('https://www.youtube.com/watch?v=' + yt_id)
+    # except pytube.exceptions.RegexMatchError:
+    #     print('Error: Could not download audio stream. Invalid YouTube video id '\
+    #      + yt_id, file=sys.stderr)
+    #     return ''
+    #
+    # #TODO: Determine which stream to prioritize; Error handling.
+    # stream = yt.streams.filter(only_audio=True).first()
+    #
+    # if stream:
+    #     stream.download(audio_dir, yt_id)
+    #     audio_path = audio_dir.rstrip('/') + '/' + yt_id + '.' \
+    #                  + get_stream_extension(stream)
+    # else:
+    #     #Download and extract audio from video stream.
+    #     #TODO: Determine which stream to prioritize; Error handling.
+    #     stream = yt.streams.first()
+    #     stream.download(video_dir, yt_id)
+    #     video_path = video_dir.rstrip('/') + '/' + yt_id + '.' \
+    #                  + get_stream_extension(stream)
+    #     audio_path =  audio_dir.rstrip('/') + '/' + yt_id + '.' + get_stream_extension(stream)
+    #     try:
+    #         subprocess.run(['ffmpeg', '-i', video_path, '-vn', '-y', '-acodec', 'copy', \
+    #         audio_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    #     except subprocess.CalledProcessError:
+    #         return ''
+    #     finally:
+    #         remove(video_path)
+    #
+    # if is_audio_valid(audio_path):
+    #     return audio_path
+    # else:
+    #     #TODO: Retries
+    #     return ''
 
 def get_stream_extension(stream):
     return stream.mime_type.split('/')[1]
