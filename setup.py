3a8853fb617558a37c2dfc8dd238df2409937677
Derek Fukumori <derekf@researcher6.fnf.archive.org>
2018-04-20 20:18:21 +0000

    Adding linux compilation flags to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import sys
 extra_compile_args = []
 if sys.platform.startswith('darwin'):
 	extra_compile_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.9']
+elif sys.platform.startswith('linux'):
+	extra_compile_args = ['-std=c++11']
 
 chromaprint_ext = Extension('chromaprint_compare_c',
                     sources = ['audiofp/chromaprint/chromaprint_compare.cpp', 'audiofp/chromaprint/include/fingerprint_matcher.cpp'],
