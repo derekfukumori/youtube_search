8d685bb579ba2ff5d60e7a2b41f5e2d146d80ab5
Derek Fukumori <derekfukumori@Dereks-MacBook-Pro.local>
2018-05-09 17:34:49 -0700

    Initial implementation of preferred-format downloading for IATracks.

--- ytsearch/iametadata.py
+++ ytsearch/iametadata.py
@@ -1,16 +1,61 @@
 import internetarchive
-import os.path
 import re
+import enum
+from os.path import splitext
 
-audio_extensions = ['.mp3', '.flac', '.ogg', '.m4a']
 
+class AudioFiletype(enum.Enum):
+    """Enum representing audio filetypes. Ordering represents the preferred
+    download format for audio fingerprinting, from most to least desirable.
+    """
+    MP3 = enum.auto(),
+    OGG = enum.auto(),
+    FLAC = enum.auto(),
+    M4A = enum.auto()
+
+def filename_to_audio_filetype(filename):
+    """ Given a filename, return the AudioFiletype value corresponding to the
+    file's extension. If the file is not one of the enumerated types, return None.
+    """
+    ext = splitext(filename)[1]
+    if ext == '.mp3':
+        return AudioFiletype.MP3
+    elif ext == '.ogg':
+        return AudioFiletype.OGG
+    elif ext == '.flac':
+        return AudioFiletype.FLAC
+    elif ext == '.m4a':
+        return AudioFiletype.M4A
+
+def get_track_duration(track_md):
+    """ Get the duration of a track in seconds given the track's metadata.
+    If no valid duration format is present, return zero.
+    """
+    if 'length' in track_md:
+        # Seconds as a float
+        match = re.match('^\d+(\.\d*)?$', track_md['length'])
+        if match:
+            return float(track_md['length'])
+        #TODO: Do durations ever take the form HH:MM:SS?
+        # MM:SS
+        match = re.match('(\d+):(\d{2})$', track_md['length'])
+        if match:
+            return 60*float(match.group(1)) + float(match.group(2))
+    return 0
 
 def get_track_ordinal(track_md):
+    """ Get the ordinal of a track representing its placement on an album, given
+    the track's metadata. If no ordering is specified in metadata, use the
+    numerals in the track's filename.
+    NB: This (usually) fails for multidisc items.
+    """
     ordinal = track_md['track'] if 'track' in track_md else track_md['name']
     ordinal = re.sub('[^0-9]+', '', ordinal)
     return int(ordinal) if ordinal else 0
 
 def get_item_artist(item_md):
+    """ Get the primary artist of an item, given the item's metadata
+    """
     artist = ''
     if 'artist' in item_md:
         if isinstance(item_md['artist'], str):
@@ -28,59 +73,44 @@ class IATrack:
     def __init__(self, metadata):
         self.metadata = metadata
         self.name = metadata['name']
+        self.orig_filetype = filename_to_audio_filetype(self.name)
         self.title = metadata['title']
-        self.artist = metadata['artist']
+        self.artist = metadata['artist'] if 'artist' in metadata else metadata['creator']
         self.album = metadata['album']
-        self.length = float(metadata['length'])
+        self.length = get_track_duration(metadata)
         self.ordinal = get_track_ordinal(metadata)
-        self.acoustid = ''
-        if 'external-identifier' in metadata:
-            if isinstance(metadata['external-identifier'], str):
-                if 'acoustid' in metadata['external-identifier']:
-                    self.acoustid = metadata['external-identifier'][13:]
-            else:
-                for identifier in metadata['external-identifier']:
-                    if 'acoustid' in identifier:
-                        self.acoustid = identifier[13:]
-    def __index__(self, index):
-        return self.metadata[index]
+        self.derivatives = {}
+        
+    def add_derivative(self, filetype, filename):
+        self.derivatives[filetype] = filename
 
+    def get_dl_filename(self):
+        """ Get the filename of the preferred download format for this track.
+        """
+        for ftype in AudioFiletype:
+            if ftype == self.orig_filetype:
+                return self.name
+            elif ftype in self.derivatives:
+                return self.derivatives[ftype]
 
 class IAItem:
     def __init__(self, iaid):
         self.item = internetarchive.get_item(iaid)
         self.artist = get_item_artist(self.item.item_metadata['metadata'])
-        self.tracks = dict((md['name'], IATrack(md)) for md in self.item.files
-                      if md['source'] == 'original'
-                      and os.path.splitext(md['name'])[1] in audio_extensions)
+        self.tracks = dict((file_md['name'], IATrack(file_md)) for file_md in self.item.files
+                      if file_md['source'] == 'original'
+                      and filename_to_audio_filetype(file_md['name']))
+
+        # Associate track objects with corresponding non-sample derivative audio files.
+        for file_md in self.item.files:
+            filetype = filename_to_audio_filetype(file_md['name'])
+            if filetype and file_md['source'] == 'derivative' \
+            and file_md['original'] in self.tracks \
+            and not splitext(file_md['name'])[0].endswith('_sample'):
+                self.tracks[file_md['original']].add_derivative(filetype, file_md['name'])
+                
         self.album = list(self.tracks.values())[0].album
-        self.length = 0
-        for track in self.tracks.values():
-            self.length += track.length
+        self.length = sum(track.length for track in self.tracks.values())
+    
     def metadata(self):
         return self.item.item_metadata
-
-
-
-
-def get_original_audio_files(item):
-    return [f for f in item.item_metadata['files'] if f['source'] == 'original'\
-            and os.path.splitext(f['name'])[1] in audio_extensions]
-
-def get_file_metadata(item, filename):
-    for file_metadata in item.item_metadata['files']:
-        if file_metadata['name'] == filename:
-            return file_metadata
-    return {}
-
-def get_acoustid(file_metadata):
-    acoustid_str = ''
-    if 'external-identifier' in file_metadata:
-        if isinstance(file_metadata['external-identifier'], str):
-            if 'acoustid' in file_metadata['external-identifier']:
-                acoustid_str = file_metadata['external-identifier'][13:]
-        else:
-            for identifier in file_metadata['external-identifier']:
-                if 'acoustid' in identifier:
-                    acoustid_str = identifier[13:]
-    return acoustid_str
