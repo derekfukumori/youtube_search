e0e5c013b0b9820d11a9bb7a6642dd47d48a0ef0
Derek Fukumori <derekfukumori@archive.org>
2018-08-30 16:06:40 -0400

    Error handling for dark items

--- exceptions.py
+++ exceptions.py
@@ -3,13 +3,17 @@ import enum
 class ExitCodes(enum.Enum):
 	IAMetadataError = 2,
 	IAMediatypeError = 3,
-	ConnectionError = 4
+	DarkedError = 4,
+	ConnectionError = 5
 
 class DownloadException(Exception):
     pass
 
 class MetadataException(Exception):
     pass
+
+class DarkedError(Exception):
+	pass
 
 class MediaTypeException(Exception):
     pass
