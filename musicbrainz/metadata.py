4fef915bc31046357fe8bb91a0f099a6937f2885
Derek Fukumori <derekfukumori@archive.org>
2018-09-14 18:12:59 -0400

    Handling edge cases for extraction of artists and publisher from musicbrainz metadata

--- musicbrainz/metadata.py
+++ musicbrainz/metadata.py
@@ -1,15 +1,26 @@
 from metadata.music_metadata import Album, Track
 
+def get_artists(mb_md):
+	# MusicBrainz artist-credit lists are sometimes structured as e.g. 
+	# [{artist-object}, 'feat.', {artist-object}, ...], so we skip over 
+	# any list members that are strings rather than dicts.
+	return [a['artist']['name'] for a in mb_md['artist-credit'] if not isinstance(a, str)]
+
+# TODO: Return catalog number as well?
+def get_publisher(mb_release_md):
+	# TODO: What are the cases for more than one publisher, and are they useful/important?
+	for mb_label_md in mb_release_md.get('label-info-list', []):
+		if 'label' in mb_label_md:
+			return mb_label_md['label']
+	return None
+
 class MusicBrainzRelease(Album):
 	def __init__(self, mb_release_md):
 		self.source = 'mb_release_id'
 		self.id = mb_release_md['id']
-		# MusicBrainz artist-credit lists are sometimes structured as e.g. [{artist-object}, 'feat.', {artist-object}, ...],
-		# so we skip over any list members that are strings rather than dicts.
-		self.artists = [a['artist']['name'] for a in mb_release_md['artist-credit'] if not isinstance(a, str)]
+		self.artists = get_artists(mb_release_md)
 		self.title = mb_release_md['title']
-		# TODO: What are the cases for more than one publisher, and are they useful/important?
-		self.publisher = mb_release_md['label-info-list'][0]['label']['name'] if mb_release_md['label-info-count'] else None
+		self.publisher = get_publisher(mb_release_md)
 		self.date = mb_release_md.get('date', None)
 		# NB: This is a list of MusicBrainzRecording objects, whose IDs are
 		# MusicBrainz recording IDs. MusicBrainz also has a 'track' structure,
@@ -29,9 +40,7 @@ class MusicBrainzRecording(Track):
 	def __init__(self, mb_recording_md):
 		self.source = 'mb_recording_id'
 		self.id = mb_recording_md['id']
-		# MusicBrainz artist-credit lists are sometimes structured as e.g. [{artist-object}, 'feat.', {artist-object}, ...],
-		# so we skip over any list members that are strings rather than dicts.
-		self.artists = [a['artist']['name'] for a in mb_recording_md['artist-credit'] if not isinstance(a, str)]
+		self.artists = get_artists(mb_recording_md)
 		self.title = mb_recording_md['title']
 		self.duration = int(mb_recording_md['length'])/1000 if 'length' in mb_recording_md else None
 		# Ordinal can't be determined from recording metadata directly, so we set
